c5ec187a4fdc6582244eaa21b3f29091a3fe7d99
Karim SABRA <ksabra@lyoelectrosrv01.in2p3.fr>
2026-07-01 16:18:00 +0200

    Add route checkpoint delete probe artifacts for 20260701_mptdc_route_ckpt_delete_probe_161359

---
+++ debug_artifacts/innovus/20260701_mptdc_route_ckpt_delete_probe_161359/commands/20260701_mptdc_route_ckpt_delete_probe_161359.commands.tcl
@@ -0,0 +1,18 @@
+editDelete -net {u_core_ro_fast_code_q[0]}
+editDelete -net {u_core_ro_fast_code_q[6]}
+editDelete -net {u_core_ro_fast_code_q[7]}
+editDelete -net {CTS_6}
+editDelete -net {u_core_n_64592}
+editDelete -net {u_core_gen_pd_row[1].gen_pd_col[1].u_pd/q1}
+editDelete -net {u_core_n_57562}
+help routeDesign
+help ecoRoute
+help globalDetailRoute
+help detailRoute
+help setNanoRouteMode
+help selectNet
+help deselectAll
+help editDelete
+help editAddRoute
+help routeSelectedNet
+help reportRoute
